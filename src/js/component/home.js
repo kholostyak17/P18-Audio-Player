88211747d37e5d709b879f0b43620cfe7055c1fa
kholostyak17 <kholostyak17@gmail.com>
2021-05-10 15:12:09 +0000

    added warning high volume

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,5 +1,12 @@
 import React from "react";
 import Player from "./player.js";
 export function Home() {
-	return <Player />;
+	return (
+		<div>
+			<div className="bg-warning font-weight-bold d-flex justify-content-center py-1">
+				<span>CUIDADO CON EL VOLUMEN INICIAL!!!!!</span>
+			</div>
+			<Player />
+		</div>
+	);
 }
